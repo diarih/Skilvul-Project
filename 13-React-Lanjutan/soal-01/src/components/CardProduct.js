dc7eb76c9c2b91a4533ffaf7d1e0c35910caa0ac
diarih <diarihza@gmail.com>
2021-11-01 19:56:15 +0700

    Done Number 1

--- 13-React-Lanjutan/soal-01/src/components/CardProduct.js
+++ 13-React-Lanjutan/soal-01/src/components/CardProduct.js
@@ -1,27 +1,41 @@
 const CardProduct = ({ name, image, price, addToCart }) => {
-    return (
-      <div className="card w-100">
-        <img src={image} className="card-img-top" alt="skilvul" />
-        <div className="card-body">
-          <h5 className="card-title">
-            {new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD"
-            }).format(price)}
-          </h5>
-          <h6 className="card-subtitle mb-2 text-muted text-capitalize">
-            {name}
-          </h6>
-          <button
-            type="button"
-            className="btn btn-primary w-100"
-            onClick={addToCart}
-          >
-            add to cart
-          </button>
-        </div>
+  return (
+    <div className="card w-100">
+      <img src={image} className="card-img-top" alt="skilvul" />
+      <div className="card-body">
+        <h5 className="card-title">
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(price)}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted text-capitalize">
+          {name}
+        </h6>
+        <button
+          type="button"
+          className="btn btn-primary w-100"
+          onClick={addToCart}
+        >
+          add to cart
+        </button>
       </div>
-    );
-  };
-  
-  export default CardProduct;
+    </div>
+  );
+};
+
+CardProduct.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  image: PropTypes.string,
+  addToCart: propTypes.function,
+};
+
+CardProduct.defaultProps = {
+  name: "",
+  price: "",
+  image: "",
+  addToCart: () => null,
+};
+
+export default CardProduct;
